1769aee2c139110b96d35190abdee0c5932feaa3
zxg9 <zxg9051c@gmail.com>
2018-07-17 18:15:15 +0800

    jsonp

---
+++ jsonp/jQueryjsonp.js
@@ -0,0 +1,43 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2018-06-15 13:11:44
+ * @version $Id$
+ */
+
+		
+$(function(){
+	$("#cityname").keyup(function(){
+		var city = $("#cityname").val()
+		$.ajax({
+			url: "http://toy1.weather.com.cn/search?cityname=" + city, 
+			typeo: "GET", 
+			dataType: "jsonp", 
+			scriptCharset : 'utf-8',
+			// jsonp: "cb", //设置了这个, 反而没用了???
+			// jsonpCallback: "zxg", 
+			// success: zxg,
+			success: (data) => zxg(data),
+			error: () => console.log('err')
+			// success: function(data){
+			// 		zxg(data)
+			// }, error: function(){
+			// 	alert('请求失败')
+			// }
+		})
+	})
+})
+
+function zxg(data){
+	if ($("ul")) $("ul").remove()
+		
+	var ul = $("<ul></ul>")
+	$.each(data, (i, v) => {
+		var ref = v.ref.split("~")
+		$("<li></li>")
+			.html(`${ref[2]}-${ref.pop()}-${ref.pop()}`)
+			.appendTo(ul)
+	})
+	
+	ul.appendTo($("body"))
+}
